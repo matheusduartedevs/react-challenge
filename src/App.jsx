3d8551b7cd1a5acec80552b58a9ea1e42b0e0421
unknown <matheus.duartesilva42@gmail.com>
2023-07-11 13:45:43 -0300

    renderização dos cliques na tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
 const App = () => {
+  const [list, setList] = useState([])
+
+  const handleClick = (event) => {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY
+    }
+
+    setList((prev) => [...prev, newDot])
+  }
+
   return (
-    <div>App</div>
+    <div id='page' onClick={handleClick}>
+      {list.map((item => (
+        <span className="dot"
+          style={{ left: item.clientX, top: item.clientY }}
+        />
+      )))}
+
+    </div>
   )
 }
 
