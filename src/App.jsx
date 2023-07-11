e7a146c25f2a9a68de778e7466fe702e0abc81b2
unknown <matheus.duartesilva42@gmail.com>
2023-07-11 14:06:16 -0300

    função de desfazer implementada

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,22 @@ const App = () => {
     setList((prev) => [...prev, newDot])
   }
 
+  const handleUndo = (event) => {
+    event.stopPropagation()
+    
+    setList((prev) => {
+      if(list.length === 0 ) {
+        return
+      }
+      
+      const newArr = [...prev].slice(0, -1)
+      return newArr
+    })
+  }
+
   return (
     <div id='page' onClick={handleClick}>
+      <button onClick={handleUndo}>Desfazer</button>
       {list.map((item => (
         <span className="dot"
           style={{ left: item.clientX, top: item.clientY }}
